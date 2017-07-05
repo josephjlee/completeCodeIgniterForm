e622bb9886c921fc15aac12f265226618b040479
mohamed hussein <m.hussein1389@gmail.com>
2017-07-05 15:22:42 +0300

    add submitForm function

--- application/controllers/formController.php
+++ application/controllers/formController.php
@@ -19,6 +19,26 @@ class FormController extends CI_Controller
     {
         //load form view 
         $this->load->view('formView');
+    }
+    
+    public function submitForm()
+    {
+        //create array of all submitted data
+        
+        $submitData=array(
+        'name'=>$this->input->post('username'),
+        'email'=>$this->input->post('email'),
+        'password'=>$this->input->post('password'),
+        'gender'=>$this->input->post('gender'),
+        'marital'=>$this->input->post('maritalStatus'),
+        'lang'=>$this->input->post('lang'),
+        'addInfo'=>$this->input->post('addInfo'),
+        'resume'=>$this->input->post('resume'),
+        );
+        
+       // print_r($data);
+        $this->load->view('formSubmittedView',$submitData);
     }   
 }
+
 ?>
