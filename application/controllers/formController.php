dcd239028b631c30fd00c145ee94faa25e6d7035
mohamed hussein <m.hussein1389@gmail.com>
2017-07-05 14:39:21 +0300

    add FormController constructor

---
+++ application/controllers/formController.php
@@ -0,0 +1,19 @@
+<?php
+class FormController extends CI_Controller
+{
+    public function __construct()
+    {
+        Parent::__construct();
+        
+        //load form helper
+        $this->load->helper('form');
+        
+        //load url helper
+        $this->load->helper('url');
+        
+        //load file helper
+        $this->load->helper('file');
+    }
+     
+}
+?>
